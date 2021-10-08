f8bb1d2efe54cb7d659801153a30d6935de0a241
elifffcansu <elifcansu@icloud.com>
2021-10-09 01:32:55 +0300

    first commit

--- src/Airtable.js
+++ src/Airtable.js
@@ -20,7 +20,7 @@ const Airtable = () => {
   return (
     <section className='section section-center'>
       <div className='title'>
-        <h2>Dante Cup Products</h2>
+        <h2>Dante Cups Products</h2>
         <div className='title-underline'></div>
       </div>
       <div className='products'>
